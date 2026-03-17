a8b288307104a599bda886f495071a1bf195194d
Janusz Badelski <255557@edu.p.lodz.pl>
2026-03-17 10:58:42 +0100

    3. Dodanie pliku odp.pdf oraz scalenie gałęzi master oraz format

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -5,9 +5,9 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
     int totalRejected = 0;
 
     public int reportRejected() {
-            return totalRejected;
-        }
-    
+        return totalRejected;
+    }
+
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
