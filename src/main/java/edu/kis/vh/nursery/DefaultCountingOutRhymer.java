6757b130eb1191b384a50447661373a5d2163866
Janusz Badelski <255557@edu.p.lodz.pl>
2026-04-08 18:14:55 +0200

    17. Wygenerowanie szkieletu dokumentacji oraz uzupełnienie dla klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,10 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa implementuje prosty mechanizm przechowywania liczb
+ * działa ona na zasadzie stosu, umożliwia dodawanie elementów
+ * oraz ich usuwanie. Jej maksymalna wielkość jest ograniczona stałą CAPACITY
+ */
 public class DefaultCountingOutRhymer {
     private static final int EMPTY = -1;
     private static final int CAPACITY = 12;
@@ -10,29 +15,53 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY;
 
+    /**
+     * Dodaje element na stos
+     * @param in zmienna zawierająca liczbę którą dodaje metoda
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Sprawdza czy stos jest pusty
+     * @return zwraca czy stos jest pusty
+     */
     public boolean callCheck() {
         return total == EMPTY;
     }
 
+    /**
+     * Sprawdza czy stos jest pełny
+     * @return zwraca czy stos jest pełny
+     */
     public boolean isFull() {
         return total == FULL;
     }
 
+    /**
+     * Zwraca wartość z stosu
+     * @return zwraca ostatnią wartość z stosu lub jeśli pusty wartość domyślną
+     */
     protected int peekaboo() {
         if (callCheck())
             return DEFAULT_VALUE;
         return numbers[total];
     }
 
+    /**
+     * Sprawdza ilość elementów na stosie
+     * @return zwraca aktuaną ilość elementów
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * zwraca i usuwa ostatnią wartość z stosu
+     * @return zwraca ostatnią wartość z stosu
+     */
     public int countOut() {
         if (callCheck())
             return DEFAULT_VALUE;
